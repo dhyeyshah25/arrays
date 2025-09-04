44c3df7945815ef85cae4b890abcb0152352557c
dhyeyshah25 <dhyeys84@gmail.com>
2025-09-04 12:37:53 +0530

    Add files via upload

---
+++ array _ 3 222.CCP.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+   int x[5],i,j,t;
+   printf("enter 5 values:");
+   for(i=0;i<=4;i++)
+        scanf("%d",&x[i]);
+   printf("UNSORTED ARRAY:");
+   for(i=0;i<=4;i++)
+        printf("%d",x[i]);
+
+
+   for(i=0;i<=3;i++)
+    for(j=i+1;j<=4;j++)
+        if(x[i]>x[j])
+        {
+            t=x[i];
+            x[i]=x[j];
+            x[j]=t;
+        }
+   printf("SORTED ARRAY:");
+   for(i=0;i<=4;i++)
+    printf("%d",x[i]);
+
+}
